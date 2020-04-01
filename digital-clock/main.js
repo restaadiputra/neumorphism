26aff461fafe5d4b858f92b4d12ad37f1594d42d
Resta Adiputra <resta.adiputra@myboost.id>
2020-04-01 08:36:56 +0700

    refactor digital clock page

---
+++ digital-clock/main.js
@@ -0,0 +1,98 @@
+let currentColor = 'red'; // set first color here
+
+// Assign each button with eventListener
+const assignEventListener = () => {
+  const buttons = document.querySelectorAll('.color__item');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      handleClick(button);
+    });
+  });
+};
+
+// Handle button click for color changing
+const handleClick = button => {
+  let active = button.classList.contains('color--active');
+  if (!active) {
+    let prevColor = currentColor;
+    currentColor = button.dataset.button;
+    changeColor(prevColor, currentColor);
+  }
+};
+
+const changeColor = (prevColor, curColor) => {
+  // Change color in 'day' box
+  let dayBox = document.querySelectorAll('.day-box__item');
+  dayBox.forEach(e => {
+    if (!!prevColor) {
+      e.classList.toggle(`text--${prevColor}`);
+    }
+    if (!!curColor) {
+      e.classList.toggle(`text--${curColor}`);
+      if (e.classList.contains(`text--${prevColor}-active`)) {
+        e.classList.remove(`text--${prevColor}-active`);
+        e.classList.add(`text--${curColor}-active`);
+      }
+    }
+  });
+
+  // Change color in 'button' box
+  let buttonBox = document.querySelectorAll('.color__item');
+  buttonBox.forEach(e => {
+    if (e.dataset.button === curColor) {
+      e.classList.add('color--active');
+      e.classList.add(`text--${e.dataset.button}-active`);
+      e.classList.remove(`text--${e.dataset.button}`);
+    } else {
+      e.classList.remove('color--active');
+      e.classList.remove(`text--${e.dataset.button}-active`);
+      e.classList.add(`text--${e.dataset.button}`);
+    }
+  });
+
+  // Change clock color
+  let clock = document.getElementById('clock-box__content');
+  clock.classList.remove(`text-clock--${prevColor}`);
+  clock.classList.add(`text-clock--${curColor}`);
+
+  // Change clock state color
+  let clockState = document.getElementById('clock-box__state');
+  clockState.classList.remove(`text-clock--${prevColor}`);
+  clockState.classList.add(`text-clock--${curColor}`);
+};
+
+// Set Time
+const setTime = () => {
+  let time = new Date();
+  let rawHour = time.getHours();
+  let state = rawHour >= 12 ? 'PM' : 'AM';
+
+  let hour = ('0' + (rawHour % 12)).slice(-2);
+  let minute = ('0' + time.getMinutes()).slice(-2);
+  let second = ('0' + time.getSeconds()).slice(-2);
+
+  let dayBox = document.querySelectorAll('.day-box__item');
+  let day = time.getDay();
+  if (!dayBox[day].classList.contains(`active-${currentColor}`)) {
+    dayBox.forEach((e, index) => {
+      if (index === day) {
+        e.classList.add(`text--${currentColor}-active`);
+      } else {
+        e.classList.remove(`text--${currentColor}-active`);
+      }
+    });
+  }
+
+  let result = hour + ':' + minute + ':' + second;
+  document.getElementById('clock-box__content').innerText = result;
+  document.getElementById('clock-box__content').textContent = result;
+  document.getElementById('clock-box__state').innerText = state;
+  document.getElementById('clock-box__state').textContent = state;
+
+  setTimeout(setTime, 1000); // Re-call setTime function in 1000ms
+};
+
+// Execution
+assignEventListener();
+changeColor('', currentColor);
+setTime();
